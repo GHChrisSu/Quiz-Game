8f70443ab1ec45633e0305a96f71fc2f046e4731
ChrisShadow <249674239@qq.com>
2017-12-31 13:45:29 +0800

    quiz game part1

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class GameController : MonoBehaviour {
+    public Text questionText;
+    public Text scoreDisplayText;
+    public Text timeRemainingDisplayText;
+    public SimpleObjectPool answerObjectPool;
+    public Transform answerButtonParent;
+    public GameObject questionDisplay;
+    public GameObject roundEndDisplay;
+
+    private DataController dataController;
+    private RoundData currentRoundData;
+    private QuestionData[] questionPool;
+
+    private bool isRoundActive;
+    private float timeRemaining;
+    private int questionIndex;
+    private int playerScore;
+    private List<GameObject> answerButtonGameObjects = new List<GameObject>();
+
+	// Use this for initialization
+	void Start () {
+        dataController = FindObjectOfType<DataController>();
+        currentRoundData = dataController.GetCurrentRoundData();
+        questionPool = currentRoundData.questions;
+        timeRemaining = currentRoundData.timeLimitInSeconds;
+        UpdateTimeRemainingDisplay();
+
+        playerScore = 0;
+        questionIndex = 0;
+
+        ShowQuestion();
+        isRoundActive = true;
+	}
+	
+    private void ShowQuestion()
+    {
+        RemoveAnswerButtons();
+        QuestionData questionData = questionPool[questionIndex];
+        questionText.text = questionData.questionText;
+        for (int i = 0; i < questionData.answers.Length; i++)
+        {
+            GameObject answerButtonGameObject = answerObjectPool.GetObject();
+            answerButtonGameObject.transform.SetParent(answerButtonParent);
+            answerButtonGameObjects.Add(answerButtonGameObject);
+            AnswerButton answerButton = answerButtonGameObject.GetComponent<AnswerButton>();
+            answerButton.Setup(questionData.answers[i]);
+        }
+    }
+
+    private void RemoveAnswerButtons()
+    {
+        while (answerButtonGameObjects.Count > 0)
+        {
+            answerObjectPool.ReturnObject(answerButtonGameObjects[0]);
+            answerButtonGameObjects.RemoveAt(0);
+        }
+    }
+
+    public void AnswerButtonClicked(bool isCorrect)
+    {
+        if (isCorrect)
+        {
+            playerScore += currentRoundData.pointsAddedForCorrectAnswer;
+            scoreDisplayText.text = "Score: " + playerScore;
+        }
+        if (questionPool.Length > questionIndex + 1)
+        {
+            questionIndex++;
+            ShowQuestion();
+        }
+        else
+        {
+            EndRound();
+        }
+    }
+
+    private void EndRound()
+    {
+        isRoundActive = false;
+        questionDisplay.SetActive(false);
+        roundEndDisplay.SetActive(true);
+    }
+
+    public void ReturnToMenu()
+    {
+        SceneManager.LoadScene("MenuScreen");
+    }
+
+    private void UpdateTimeRemainingDisplay()
+    {
+        timeRemainingDisplayText.text = "Time: " + Mathf.Round(timeRemaining);
+    }
+
+    // Update is called once per frame
+    void Update () {
+        if (isRoundActive)
+        {
+            timeRemaining -= Time.deltaTime;
+            UpdateTimeRemainingDisplay();
+
+            if (timeRemaining <= 0)
+            {
+                EndRound();
+            }
+        }
+	}
+}
---
+++ Assets/Scripts/SimpleObjectPool.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimpleObjectPool : MonoBehaviour {
+
+    public GameObject prefab;
+    private Stack<GameObject> inactiveInstances = new Stack<GameObject>();
+
+    public GameObject GetObject()
+    {
+        GameObject spawnedGameObject;
+        if (inactiveInstances.Count>0)
+        {
+            spawnedGameObject = inactiveInstances.Pop();
+        }
+        else
+        {
+            spawnedGameObject = Instantiate(prefab) as GameObject;
+
+            //add the PooledObject component to the prefab so we know it came from this pool
+            PooledObject pooledObject = spawnedGameObject.AddComponent<PooledObject>();
+            pooledObject.pool = this;
+        }
+
+        //put the instance in the root of the scene and enable it
+        spawnedGameObject.transform.SetParent(null);
+        spawnedGameObject.SetActive(true);
+
+        return spawnedGameObject;
+    }
+
+    //return an instance of the prefab to the pool
+    public void ReturnObject(GameObject toReturn)
+    {
+        PooledObject pooledObject = toReturn.GetComponent<PooledObject>();
+
+        //if the instance came from this pool, return it to the pool
+        if (pooledObject != null && pooledObject.pool == this)
+        {
+            toReturn.transform.SetParent(transform);
+            toReturn.SetActive(false);
+
+            // add the instance to the collection of inactive Instance
+            inactiveInstances.Push(toReturn);
+        }
+        else
+        {
+            Debug.LogWarning(toReturn.name + "was returned to a pool it wasn't spawned from! Destroying.");
+            Destroy(toReturn);
+        }
+    }
+}
+
+// a component that simply identifies the pool that a GameObject came from
+public class PooledObject : MonoBehaviour
+{
+    public SimpleObjectPool pool;
+}
